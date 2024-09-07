c2ad2426bb658dc4a898d46aec6d03931f19713d
jacson-costa <92768264+jacson-costa@users.noreply.github.com>
2024-09-07 10:20:33 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Ronaldo Fenômento",
+        descricao: "Ronaldo Luís Nazário de Lima, mais conhecido como Ronaldo Fenômeno, é um dos maiores jogadores de futebol de todos os tempos. Com uma habilidade única para driblar, velocidade impressionante e um faro de gol inigualável, Ronaldo encantou milhões de fãs ao redor do mundo",
+        link : "https://pt.wikipedia.org/wiki/Ronaldo_Naz%C3%A1rio",
+        tags : "Lenda do Futebol, Craque, Copa do Mundo, Ídolo Nacional"
+    }, 
+    {
+        titulo : "Ronaldinho Gaucho",
+        descricao: "RÍcone do futebol mundial, conhecido por seu estilo de jogo alegre e habilidoso. Campeão da Copa do Mundo de 2002, Ronaldinho encantou a todos com seus dribles desconcertantes e gols de placa. Sua passagem pelo Barcelona foi marcante, onde conquistou a Liga dos Campeões e o prêmio de melhor jogador do mundo.",
+        link : "https://pt.wikipedia.org/wiki/Ronaldinho",
+        tags : "bruxo, drible, barcelona, mudial"
+    },
+    {
+        titulo : "Romario",
+        descricao: "Um dos maiores artilheiros da história do futebol, Romário se destacou por sua frieza na finalização e habilidade para marcar gols. Campeão mundial em 1994, ele conquistou títulos importantes por clubes como Vasco, Flamengo, PSV Eindhoven e Barcelona. Sua frase Eu sou o Romário, e faço gols se tornou um símbolo de sua confiança e sucesso.",
+        link : "https://pt.wikipedia.org/wiki/Rom%C3%A1rio",
+        tags : "baixinho, artilheiro, mundial"
+    },  
+];
